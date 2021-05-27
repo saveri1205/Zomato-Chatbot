635abbb89741bc1c00ffa4e5da34c417ee26afa1
saveri1205 <54893537+saveri1205@users.noreply.github.com>
2021-05-27 12:16:12 +0530

    Add files via upload

--- GUI/Code.py
+++ GUI/Code.py
@@ -1,1 +1,186 @@
+import re
+import numpy as  np
+import pandas as pd
+def differentiate(reply):
+    reply_list=reply.split()
+    for i in ['can','could','might','should','say','ask','must','tell']:
+        if i in reply_list:
+                return True
+    return False
 
+def review_generator(number,context):
+    df=pd.read_csv('zomato.csv',encoding="ISO-8859-1", low_memory=False)
+    d=0
+    #print(df['reviews_list'][:10])
+    p=re.compile(r'\b(most|worst|worse|best|top|least)\b')
+    q=p.search(context)
+    word=None
+    if(q):
+        q=q.span()
+        word=context[q[0]:q[1]]
+        #print(word)
+        flag=context.split().index(word)
+        while "not"==context.split()[flag-1]:
+            flag-=1
+            d+=1
+    d%=2
+    l=list(df['name'])
+    l=[x.lower() for x in l]
+    flag=0
+    for i in context.split():
+        if i in l:
+            flag=1
+            break
+    if(flag==0):
+        return "In a specific hotel only we can provide the reviews."
+    reviews=df['reviews_list'][l.index(i)].lstrip("[").rstrip("]")
+    #print(reviews,'\n',type(reviews))
+    res = []
+    temp = []
+    for token in reviews.split(", "):
+        st= token.replace("(", "").replace(")", "")
+        temp.append(st.strip("\"").strip("'"))
+        if ")" in token:
+            res.append(tuple(temp))
+            temp = []
+    reviews=res
+    #print(reviews[:5])
+    if(reviews==None):
+        return "Sorry,for this restaurant the data is not available."
+    flag2=1
+    if("rating" in context.split()):
+        #print("kill")
+        num=context.split()[context.split().index("rating")+1]
+        if(num.isnumeric()):
+            num=int(num)
+        else:
+            numb=re.compile(r"\b(one|two|three|four|five)\b")
+            dic={'one':1,'two':2,'three':3,'four':4,'five':5}
+            p=numb.match(num)
+            if(p==None):
+                flag2=1
+            else:
+                num=dic[num]
+                flag2=0
+        if(int(num)>5):
+            return "They are rated out of 5"
+    reviews_2=[]
+    if(flag2==0 and d==0):
+        reviews_2=[]
+        y=number
+        for i in reviews:
+            p='Rated'+' '+str(float(num))
+            if(i[0]==p):
+                reviews_2.append(i[1])
+                y-=1
+            if(y<=0):
+                break
+    if(((word=="top" or word=="best")and d==0) or ((word=="least" or word=="worst")and d==1)):
+        y=number
+        for i in reviews:
+            p='Rated'+' '+str(float(5))
+            #print(i)
+            if(i[0]==p):
+                reviews_2.append(i[1])
+                y-=1
+            if(y<=0):
+                break
+    if(((word=="top" or word=="best")and d==1) or ((word=="least" or word=="worst")and d==0)):
+        y=number
+        for i in reviews:
+            p='Rated'+' '+str(float(1))
+            if(i[0]==p):
+                reviews_2.append(i[1])
+                y-=1
+            if(y<=0):
+                break
+    s=''
+    j=0
+    for i in reviews_2:
+        l=i.split("\n")
+        p=[]
+        for k in l:
+            k=k[k.index("\\n"):].replace("\\n","\n").strip("\n")
+            p.append(k)
+        #print(l)
+        p=" ".join(p)
+        s=s+str(j)+". "+p
+        j+=1
+    return s
+        
+    
+
+def generate(reply):
+    l=reply.split()
+    for i in l:
+        num=re.compile(r"\b(one|two|three|four|five|six|seven|eight|nine|ten)\b")
+        dic={'one':1,'two':2,'three':3,'four':4,'five':5,'six':6,'seven':7,'eight':8,'nine':9,'ten':10}
+        p=num.search(i)
+        if(i.isnumeric() or p):
+            if(p!=None):
+                s=dic[i]
+            else:
+                s=int(i)
+            if(int(i)>10):
+                return "Too much to be readable"
+    if("reviews" in l):
+        response=review_generator(s,reply)
+        if(response==''):
+            return ("Information is not available.")
+        return response
+    return ("Can you explain properly like what you want!")
+
+
+def extract_answer(reply):
+    if(differentiate(reply)):
+        l=reply.split()
+        if "ask" in l:
+            pronouns=re.compile(r"\b(h+(im|er|e))\b|\bshe\b|\bthe person\b")
+            ch=pronouns.search(reply).span()
+            l=reply[0:ch[0]-1]+" you"+reply[ch[1]:]
+            l=l.split()
+            if "." in l:
+                l.remove(".")
+            elif "." in l[-1]:
+                l[-1]=l[-1][0:len(l[-1])-1]
+            ind=l.index('ask')
+            if l[ind+1]=="whether":
+                ques=l[ind+1:]
+                ques[0]="if"
+                ques.append("?")
+                return (" ".join(ques),1)
+            elif l[ind+1]=="about" or l[ind+1]=="that":
+                ques=l[ind+2:]
+                ques.append("?")
+                return (" ".join(ques),1)
+    ques_patt=re.compile(r"\b((wh+(o|a+t|ere|y|en))|how)\b")
+    matche=ques_patt.search(reply)
+    if(matche!=None):
+                if "." in reply:
+                    reply=reply[0:len(reply)-1]
+                p=matche.span()[0]
+                q=reply[p:].split()[1]
+                pronouns=re.compile(r"\b(h+(im|er|e))\b|\bshe\b|\bthe person\b")
+                ch=pronouns.search(reply[p:]).span()
+                verb=reply[p:][ch[0]:].split()[1]
+                #print(verb)
+                ques=reply[matche.span()[0]:matche.span()[1]]+" do you "+reply[p:][(ch[1]+len(verb)+(ch[1]-ch[0])):]
+                ques+='?'
+                return (" ".join(ques),2)
+    fi=r"\b(find|display|show)\b"
+    pr=r"\b(top|most|least|worst)\b"
+    p=re.search(fi,reply)
+    q=re.search(pr,reply)
+    if(p!=None):
+        if(q!=None):
+            l=generate(reply)
+            return (l,4)
+        else:
+            return (reply[p.span()[0]:],4)
+    else:
+        return(reply,3)
+#print(extract_answer("you should ask where he wants to have coffee.".lower()))
+#print(extract_answer("You can ask whether he want to have coffe.".lower()))
+#print(extract_answer("You should display top 10 reviews in Jalsa".lower()))
+#print(extract_answer("when he does like to have coffe".lower()))
+#print(extract_answer("You should display top 10 reviews rating 4 in Jalsa".lower()))
--- GUI/clean_ui.py
+++ GUI/clean_ui.py
@@ -1,1 +1,177 @@
+import tkinter
+from tkinter import *
+#from chat import get_response, bot_name
+import nltk
+from nltk.stem import WordNetLemmatizer
+import pickle
+import numpy as np
+from keras.models import load_model
+import json
+import random
+from tkinter import simpledialog; 
+from Code import extract_answer
+BG_GRAY = "#ABB2B9"
+BG_COLOR = "#17202A"
+TEXT_COLOR = "#EAECEE"
 
+FONT = "Helvetica 14"
+FONT_BOLD = "Helvetica 13 bold"
+
+
+lemmatizer = WordNetLemmatizer()
+model = load_model('chatbot.h5')
+
+with open('zomato-data.json') as jsonfile:
+    intents = json.load(jsonfile)
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
+
+def clean_up_sentence(sentence):
+    # tokenize the pattern - split words into array
+    sentence_words = nltk.word_tokenize(sentence)
+    # stem each word - create short form for word
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getResponse(ints, intents_json):
+    if(len(ints)>0):
+        tag = ints[0]['intent']
+        list_of_intents = intents_json['intents']
+        for i in list_of_intents:
+            if(i['tag']== tag):
+                result = random.choice(i['response'])
+                break
+    else:
+        result="Sorry, we don't have the answer to the question.\n"
+    return result
+
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    return res
+
+
+
+
+class ChatApplication:   
+    def __init__(self):
+        self.window = Tk()
+        self._setup_main_window()
+
+    def run(self):
+        self.window.mainloop()
+
+    def _setup_main_window(self):
+        self.window.title("Restaurant Based Chatbot")
+        self.window.resizable(width=False, height=False)
+        self.window.configure(width=470, height=550, bg=BG_COLOR)
+        
+        head_label = Label(self.window, bg=BG_COLOR, fg=TEXT_COLOR,text="This is a restaurant based chatbot for Bangalore!", font=FONT_BOLD, pady=10)
+        head_label.place(relwidth=1)
+        
+        line = Label(self.window, width=450, bg=BG_GRAY)
+        line.place(relwidth=1, rely=0.07, relheight=0.012)
+        
+        self.text_widget = Text(self.window, width=20, height=2, bg=BG_COLOR, fg=TEXT_COLOR,
+                                font=FONT, padx=5, pady=5)
+        self.text_widget.place(relheight=0.745, relwidth=1, rely=0.08)
+        self.text_widget.configure(cursor="arrow", state=DISABLED)
+        
+        scrollbar = Scrollbar(self.text_widget)
+        scrollbar.place(relheight=1, relx=0.974)
+        scrollbar.configure(command=self.text_widget.yview)
+        
+        bottom_label = Label(self.window, bg=BG_GRAY, height=80)
+        bottom_label.place(relwidth=1, rely=0.825)
+
+        self.msg_entry = Entry(bottom_label, bg="#2C3E50", fg=TEXT_COLOR, font=FONT)
+        self.msg_entry.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
+        self.msg_entry.focus()
+        self.msg_entry.bind("<Return>", self.send)
+    
+    def send(self,event):
+        msg = self.msg_entry.get()
+        self.msg_entry.delete(0, END)
+        print("first: ",msg)
+        chat_history.append(msg)
+        print(chat_history)
+        if msg != '':
+            msg1 = f"You: {msg}\n\n"
+            self.text_widget.configure(state=NORMAL)
+            self.text_widget.insert(END, msg1)
+
+            if(msg=='no'):
+                res ="Please enter an alternate response"
+                alt_response = simpledialog.askstring("Prompt","Please enter a valid response")
+                #self.text_widget.insert(END, "Bot: Your alternate response to the question \' "+chat_history[-2]+"\' is:  " + alt_response + '\n\n')
+                self.text_widget.insert(END, "Thank you, I have recorded your input! Please contine with your next question!\n\n") 
+                fp=open("Logged_resonses.json","r")
+                response=extract_answer(alt_response.lower())
+                if(response[1]!=3):
+                    self.text_widget.insert(END, "Bot: "+response[0]+"\n\n")
+                    self.text_widget.insert(END, "Bot: Was this response satisfactory? \n\n") 
+                #print(response)
+                data = json.load(fp)
+                new_entry=dict()
+                new_entry["tag"]=chat_history[-2]
+                new_entry["patterns"]=[chat_history[-2]]
+                new_entry["responses"]=[response[0]]
+                new_entry["context"]="user_response"
+                data["intents"].append(new_entry)
+                fp.close()
+                fp=open("Logged_resonses.json","w")
+                json.dump(data,fp,indent=4)
+                fp.close()
+                self.text_widget.configure(state=DISABLED)
+                self.text_widget.see(END)
+            elif(msg=="yes"):
+                res = "Okay, please enter your next question!"
+                self.text_widget.insert(END, "Bot: "+res+"\n\n")
+                self.text_widget.configure(state=DISABLED) 
+                self.text_widget.see(END)
+
+            else:
+                res = chatbot_response(msg)
+                self.text_widget.insert(END, "Bot: "+res+"\n\n")
+                self.text_widget.insert(END, "Bot: Was this response satisfactory? \n\n") 
+                self.text_widget.configure(state=DISABLED) 
+                self.text_widget.see(END)
+
+if __name__ == "__main__":
+    with open("Logged_resonses.json","w") as fp:
+        d=dict()
+        d["intents"]=[]
+        json.dump(d,fp,indent=4)
+        fp.close()
+    app = ChatApplication()
+    chat_history=[]
+    app.run()
